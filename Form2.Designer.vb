b471ffe6d79516aac2edb68c63f52196c6605e90
Will Jones <willjones97@hotmail.com>
2018-11-04 14:42:15 +0000

    fwwq

--- Form2.Designer.vb
+++ Form2.Designer.vb
@@ -81,10 +81,10 @@ Partial Class Form2
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.Location = New System.Drawing.Point(474, 11)
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(444, 5)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(110, 31)
+        Me.Label1.Size = New System.Drawing.Size(127, 37)
         Me.Label1.TabIndex = 6
         Me.Label1.Text = "Label11"
         '
