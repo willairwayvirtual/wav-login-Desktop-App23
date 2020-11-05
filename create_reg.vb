e3cecf94f0ff6f3018a3349a92322f4feecbac60
willairwayvirtual <40953328+willairwayvirtual@users.noreply.github.com>
2020-11-05 16:54:28 +0000

    Update create_reg.vb

--- create_reg.vb
+++ create_reg.vb
@@ -1,4 +1,4 @@
-﻿Imports System.Data.OleDb
+Imports System.Net.Mail
 Public Class create_reg
     Dim provider As String
     Dim dataFile As String
@@ -9,7 +9,7 @@ Public Class create_reg
 
         provider = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source ="
         'Change the following to your access database location
-        dataFile = "C:\VisStudioProj\wav web\willairwayvirtual34\willairwayvirtual34\app_data\willairwayvirtualDBv1.accdb"
+        dataFile = "c:\VisStudioProj\wav web\willairwayvirtual34\willairwayvirtual34\app_data\willairwayvirtualDBv1.accdb"
         connString = provider & dataFile
         conn.ConnectionString = connString
 
@@ -26,15 +26,15 @@ Public Class create_reg
         
         Else
             'add new records
-            Dim savenew As String = "INSERT INTO [tblaccessinfo]  (Uname,Pword,Fname,Lname,EMailAdd,HOMEICAO) values('" &
-        new_username.Text & "','" &
+            Dim savenew As String = "INSERT INTO [tblaccessinfo]  (Uname,Pword,Fname,Lname,EMailAdd,HOMEICAO,emailnote,lkey) values('" &
+       new_username.Text & "','" &
         new_password.Text & "','" &
         New_fname.Text & "','" &
         new_lname.Text & "','" &
         EMailAdd.Text & "','" &
-        HOMEICAO.Text & "');"
-
-
+        HOMEICAO.Text & "','" &
+        emailnote.Text & "','" &
+        lkeyTextBox1.Text & "');"
             Dim cmd As New OleDbCommand
 
             With cmd
@@ -45,6 +45,25 @@ Public Class create_reg
             MessageBox.Show("Welcome on board ")
 
             conn.Close()
+            Dim mail As New MailMessage
+            Dim body As String = String.Empty
+            Dim reader As StreamReader = New StreamReader("C:\VisStudioProj\wav login Desktop App23\wav login Desktop App23\email_template.htm")
+            Dim smtp As New SmtpClient("smtp-mail.outlook.com")
+
+            mail.To.Add(EMailAdd.Text)
+            mail.From = New MailAddress("Wavuserhelpdesk@hotmail.com")
+            mail.Body = reader.ReadToEnd
+            mail.IsBodyHtml = True
+
+            smtp.EnableSsl = True
+            smtp.Credentials = New System.Net.NetworkCredential("Wavuserhelpdesk@hotmail.com", "Spongebob97")
+            smtp.Port = "587"
+            'smtp.Port = "25"
+            smtp.Send(mail)
+
+
+
+
         End If
     End Sub
 
